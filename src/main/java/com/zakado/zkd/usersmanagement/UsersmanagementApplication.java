983df52ac3427ed64c470105e9b4ba9d42147183
micha <micha2mc@gmail.com>
2024-01-13 09:44:29 +0100

    modifs para la primeras pruebas

--- src/main/java/com/zakado/zkd/usersmanagement/UsersmanagementApplication.java
+++ src/main/java/com/zakado/zkd/usersmanagement/UsersmanagementApplication.java
@@ -2,8 +2,10 @@ package com.zakado.zkd.usersmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersmanagementApplication {
 
 	public static void main(String[] args) {
